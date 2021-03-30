3e4043d107f52ee18b503e68f578ae9f86179b12
pzyxo <hydrex.ion45@gmail.com>
2021-03-30 13:53:40 +0700

    Update prak030202.php

--- praktikum03 - Fayusri Royfanto - K3519033/prak030202.php
+++ praktikum03 - Fayusri Royfanto - K3519033/prak030202.php
@@ -1,22 +1,27 @@
 <?php
 
 function hitungDenda($tglHarusKembali, $tglKembali){
-    $pecah1 = explode("-", $tglHarusKembali);
-    $date1 = $pecah1[2];
-    $month1 = $pecah1[1];
-    $year1 = $pecah1[0];
+    #explode tanggal harus kembali
+    $tgl1 = explode("-", $tglHarusKembali);
+    $date1 = $tgl1[2];
+    $month1 = $tgl1[1];
+    $year1 = $tgl1[0];
 
-    $pecah2 = explode("-", $tglKembali);
-    $date2 = $pecah2[2];
-    $month2 = $pecah2[1];
-    $year2 = $pecah2[0];
+    #explode tanggal kembali
+    $tgl2 = explode("-", $tglKembali);
+    $date2 = $tgl2[2];
+    $month2 = $tgl2[1];
+    $year2 = $tgl2[0];
 
+    #mengubah ke format JDN
     $data1 = gregoriantojd($month1, $date1, $year1);
     $data2 = gregoriantojd($month2, $date2, $year2);
 
-    $selisih = $data2 - $data1;
+    #menghitung selisih hari
+    $selisihHari = $data2 - $data1;
 
-    $denda = $selisih * 3000;
+    #menghitung denda
+    $denda = $selisihHari * 3000;
 
     return $denda;
 }
